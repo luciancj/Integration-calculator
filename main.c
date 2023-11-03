aaca0f35e0f68bad00db17355d4bb746ab319156
Lucian Cojocaru <lucian.private@icloud.com>
2023-11-03 05:20:25 +0100

    Library cleanup in "main.c"
    Signed-off-by: Lucian Cojocaru <lucian.private@icloud.com>

--- main.c
+++ main.c
@@ -1,15 +1,5 @@
 #include "stdio.h"
-#include "user_menu.h"
-#include "im_text.h"
-#include "text_data.h"
-#include "stdlib.h"
 #include "menu_choice.h"
-
-void printGreenText(struct TextData data) {
-	for (int i = 0; i < data.numRows; i++) {
-		printf("\033[0;32m%s\033[0m\n", data.text[i]);
-	}
-}
 
 
 int main() {
